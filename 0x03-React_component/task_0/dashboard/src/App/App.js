0414378beebef6302b48e3fe30585af7b03f460a
attahchisom6 <attahchisom6@gmail.com>
2023-11-18 23:51:03 +0100

    add

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -9,53 +9,56 @@ import Footer from '../Footer/Footer';
 import CourseList from '../CourseList/CourseList';
 import PropTypes from 'prop-types';
 
-const listCourses = [
-  {
-    id: 1,
+class App extends React.Component {
+  listCourses = [
+    {
+      id: 1,
     name: "ES6",
-     credit: 60,
-  },
-  {
-    id: 2,
-    name: "Webpack",
-    credit: 20,
-  },
-  {
-    id: 3,
-    name: "React",
-    credit: 40,
-  },
-]
+      credit: 60,
+    },
+    {
+      id: 2,
+      name: "Webpack",
+      credit: 20,
+    },
+    {
+      id: 3,
+      name: "React",
+      credit: 40,
+    },
+  ];
 
-const listNotifications = [
-  {
-    id: 1,
-    type: "default",
-    value: "New course available",
-  },
-  {
-    id: 2,
-    type: "urgent",
-    value: "New resume available",
-  },
-  {
-    id: 3,
-    type: "urgent",
-    html: getLatestNotification(),
-  },
-]
+  listNotifications = [
+    {
+      id: 1,
+      type: "default",
+      value: "New course available",
+    },
+    {
+      id: 2,
+      type: "urgent",
+      value: "New resume available",
+    },
+    {
+      id: 3,
+      type: "urgent",
+      html: getLatestNotification(),
+    },
+  ];
 
-const App = ({ isLoggedIn }) => {
-  return (
-    <React.Fragment>
-      <Notifications listNotifications={ listNotifications } />
-      <div className="App">
-        <Header />
-        { isLoggedIn ? <CourseList listCourses={ listCourses } /> : <Login /> }
-        <Footer />
-      </div>
-    </React.Fragment>
-  );
+  render() {
+    return (
+      <React.Fragment>
+        <Notifications listNotifications={ this.listNotifications } />
+        <div className="App">
+          <Header />
+          { this.props.isLoggedIn ? <CourseList listCourses={ this.listCourses } /> : <Login /> }
+          <Footer />
+        </div>
+      </React.Fragment>
+    );
+  }
+
 };
 
 App.propTypes = {
